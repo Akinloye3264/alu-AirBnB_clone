d7552a024f9b64ce0f2917267d882968cc96ad83
Akinloye3264 <e.akinloye@alustudent.com>
2025-09-13 23:06:05 +0200

    Update __init__.py

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-"""
-Initializes the models package.
-"""
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
+"""engine package init"""
