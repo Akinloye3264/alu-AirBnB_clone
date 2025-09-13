d61bbbe3d96594f8c142b9c024cb6fb23ba3343d
Akinloye3264 <e.akinloye@alustudent.com>
2025-09-14 00:28:57 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,21 @@
-# models/user.py
+#!/usr/bin/python3
+"""
+This module defines the User class.
+It inherits from BaseModel.
+"""
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-    """User class that inherits from BaseModel"""
-    
+    """
+    User class that represents a user of the application.
+
+    Public class attributes:
+        email (str): The email address of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
     email = ""
     password = ""
     first_name = ""
