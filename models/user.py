2dbf51ed6042a51ebd334e07f53f5c99b281dd2a
Akinloye3264 <e.akinloye@alustudent.com>
2025-09-14 00:03:39 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""This module defines the User class"""
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Representation of a User."""
-
+    """Representation of a User"""
     email = ""
     password = ""
     first_name = ""
