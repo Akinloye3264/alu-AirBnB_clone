707266821390721d5b4ea55be56c8f50bc457372
Akinloye3264 <e.akinloye@alustudent.com>
2025-09-13 22:55:40 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,59 +1,42 @@
 #!/usr/bin/python3
-"""
-Module base_model
-Defines the BaseModel class for AirBnB clone project.
-"""
+"""BaseModel module"""
 
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """
-    Base class for all AirBnB objects.
-    Handles id, creation, update, and serialization.
-    """
+    """Defines all common attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """
-        Initialize a new BaseModel instance.
-        If kwargs is not empty, create attributes from the dictionary.
-        Otherwise, create a new instance with a unique id and timestamps.
-        """
-        if kwargs:
+        """Initialize a new BaseModel or recreate from a dictionary"""
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if kwargs and len(kwargs) > 0:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ("created_at", "updated_at"):
-                        value = datetime.fromisoformat(value)
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    setattr(self, key, datetime.strptime(value, tform))
+                else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            from models import storage
-            storage.new(self)
+            self.updated_at = self.created_at
 
     def __str__(self):
-        """
-        Return string representation of BaseModel instance.
-        """
+        """String representation of BaseModel"""
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__
         )
 
     def save(self):
-        """
-        Update updated_at timestamp and save object to storage.
-        """
+        """Update the updated_at attribute"""
         self.updated_at = datetime.now()
-        from models import storage
-        storage.save()
 
     def to_dict(self):
-        """
-        Return dictionary representation of instance,
-        converting datetime attributes to ISO format strings.
-        """
+        """Return a dictionary containing all keys/values of the instance"""
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
