d14af894ab91b572d924288a5c9f469b83207603
Akinloye3264 <e.akinloye@alustudent.com>
2025-09-13 23:05:01 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -24,6 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """String representation of BaseModel"""
@@ -32,8 +34,9 @@ class BaseModel:
         )
 
     def save(self):
-        """Update the updated_at attribute"""
+        """Update updated_at and save to storage"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Return a dictionary containing all keys/values of the instance"""
