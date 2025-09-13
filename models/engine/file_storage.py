4dadefa9f43d380af74b24c4e15431783c64dc83
Akinloye3264 <e.akinloye@alustudent.com>
2025-09-13 23:03:57 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,52 +1,45 @@
 #!/usr/bin/python3
-"""
-This module defines the FileStorage class.
-"""
+"""FileStorage module - serializes instances to JSON and deserializes back"""
+
 import json
 import os
-from models.base_model import BaseModel
 
 
 class FileStorage:
-    """
-    Serializes instances to a JSON file and deserializes JSON file to instances.
-    """
+    """Serializes instances to a JSON file and deserializes back"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Returns the dictionary __objects.
-        """
+        """Returns the dictionary __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        Sets in __objects the obj with key <obj class name>.id.
-        """
-        key = f"{obj.__class__.__name__}.{obj.id}"
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """
-        Serializes __objects to the JSON file.
-        """
-        new_dict = {}
-        for key, value in FileStorage.__objects.items():
-            new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(new_dict, f)
+        """Serializes __objects to the JSON file"""
+        obj_dict = {}
+        for key, obj in FileStorage.__objects.items():
+            obj_dict[key] = obj.to_dict()
+
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            json.dump(obj_dict, f)
 
     def reload(self):
-        """
-        Deserializes the JSON file to __objects.
-        """
+        """Deserializes the JSON file to __objects (if it exists)"""
+        from models.base_model import BaseModel  # import inside to avoid circular import
+
         if os.path.exists(FileStorage.__file_path):
             try:
-                with open(FileStorage.__file_path, 'r') as f:
+                with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
                     obj_dict = json.load(f)
-                    for key, value in obj_dict.items():
-                        class_name = value["__class__"]
-                        self.__objects[key] = eval(class_name)(**value)
+                for key, value in obj_dict.items():
+                    cls_name = value["__class__"]
+                    if cls_name == "BaseModel":
+                        FileStorage.__objects[key] = BaseModel(**value)
             except Exception:
                 pass
